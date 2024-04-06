00899c2f8faf16f3c2db35efc7cafcb097e8442c
Kaveen Abeywansa <kaveenabeywansa@gmail.com>
2024-04-06 21:52:59 +0530

    init project

---
+++ backends/quote-gen-service/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const portNo = process.env.DEFAULT_PORT_NO || 5002;
+
+const app = express();
+app.use(cors());
+app.listen(portNo, function (err) {
+    if (err) {
+        console.log(err);
+        process.exit(-1);
+    }
+    console.log('Quote Generating Service Running in Port ' + portNo);
+});
