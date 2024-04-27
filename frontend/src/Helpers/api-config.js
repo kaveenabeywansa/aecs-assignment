7eb2641caf5d302d1b83736e04fc719b9fcbaa4c
Kaveen Abeywansa <kaveenabeywansa@gmail.com>
2024-04-28 02:47:50 +0530

    Test AWS Amplify

--- frontend/src/Helpers/api-config.js
+++ frontend/src/Helpers/api-config.js
@@ -2,8 +2,10 @@ const CONST_USER_LIKE_API = 'USER_LIKE_SERVICE_API_ENDPOINT';
 const CONST_USER_QUOTE_API = 'USER_QUOTE_SERVICE_API_ENDPOINT';
 
 const BASE_URLS = {
-    [CONST_USER_LIKE_API]: 'http://localhost:5001',
-    [CONST_USER_QUOTE_API]: 'http://localhost:5002'
+    // [CONST_USER_LIKE_API]: 'http://localhost:5001',
+    // [CONST_USER_QUOTE_API]: 'http://localhost:5002'
+    [CONST_USER_LIKE_API]: 'http://16.171.129.69:4001',
+    [CONST_USER_QUOTE_API]: 'http://16.171.129.69:4002'
 };
 
 const APIConfig = function () {
