a5cd8886fb13bc4f22bccd62630039d7d72d1979
Kaveen Abeywansa <kaveenabeywansa@gmail.com>
2024-05-11 07:02:39 +0530

    Update IP

--- frontend/src/Helpers/api-config.js
+++ frontend/src/Helpers/api-config.js
@@ -4,8 +4,8 @@ const CONST_USER_QUOTE_API = 'USER_QUOTE_SERVICE_API_ENDPOINT';
 const BASE_URLS = {
     // [CONST_USER_LIKE_API]: 'http://localhost:5001',
     // [CONST_USER_QUOTE_API]: 'http://localhost:5002'
-    [CONST_USER_LIKE_API]: 'http://13.60.45.144:4001',
-    [CONST_USER_QUOTE_API]: 'http://13.60.45.144:4002'
+    [CONST_USER_LIKE_API]: 'http://13.51.197.213:4001',
+    [CONST_USER_QUOTE_API]: 'http://13.51.197.213:4002'
 };
 
 const APIConfig = function () {
